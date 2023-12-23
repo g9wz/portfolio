9359ee1977d32a531599b1e1d0e12c398698c4bc
Khalil Melhem <khalil.i.melhem@gmail.com>
2023-12-23 18:23:47 +0300

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,21 @@
 import Header from "@components/header";
 import Home from "@components/home";
 import About from "@components/about";
+import Work from "@components/work";
 
 export default function Main() {
-  // copilot generated this 👍🏻
-  const idk = [
-    { id: "work", text: "Yup, still Khalil. You know the drill" },
-    { id: "contact", text: "Still here. Still Khalil.." }
-  ];
-
   return (
     <>
       <Header />
       <Home />
       <About />
-      {idk.map(({ id, text }) => {
-        return (
-          <section id={id} key={id}>
-            <h1 className="rounded-full bg-black/50 p-7 text-center text-3xl">
-              {text} 👋🏻
-            </h1>
-          </section>
-        );
-      })}
+      <Work />
+
+      <section id="contact">
+        <h1 className="rounded-full bg-black/50 p-7 text-center text-3xl">
+          Still here. Still Khalil.. 👋🏻
+        </h1>
+      </section>
     </>
   );
 }
---
+++ src/components/work/Work.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Work = () => {
+  return (
+    <section id="work">
+      <h1 className="rounded-full bg-black/50 p-7 text-center text-3xl">
+        hi 👋🏻
+      </h1>
+    </section>
+  );
+};
+
+export default Work;
