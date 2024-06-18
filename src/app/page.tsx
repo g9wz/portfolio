3dd7d5aad1979130d49c683614aa3fe34e4c6422
Khalil Melhem <me@khalil.im>
2024-06-18 08:23:15 +0300

    add hero section and enhance header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header/Header";
+import Hero from "@/components/hero/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
