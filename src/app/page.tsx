928f2a32d76511ea7d18c4916bebef499d2efd34
Khalil Melhem <me@khalil.im>
2024-06-19 02:10:55 +0300

    add about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
+import About from "@/components/about/About";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <About />
     </>
   );
 }
---
+++ src/components/about/About.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+import { Text, Button } from "./parts";
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="-mx-5 my-10 bg-black px-5 py-10 text-white lg:mx-auto lg:my-24 lg:rounded-[64px] lg:px-14 lg:py-24 xl:px-[8%]"
+    >
+      <div className="flex flex-col items-center justify-between gap-y-20 xl:flex-row-reverse">
+        <div className="flex items-center justify-center rounded-[64px] bg-white max-xl:mx-auto xl:ml-auto xl:h-[555px]">
+          <Image
+            src="/me.png"
+            alt="My Memoji"
+            width={420}
+            height={420}
+            quality={100}
+          />
+        </div>
+        <div className="w-full space-y-10 lg:space-y-20 xl:w-[55%]">
+          <Text />
+          <Button />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
