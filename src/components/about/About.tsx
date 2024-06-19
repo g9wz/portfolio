fcf568d443d746ddcf073d7e67afef2b69dd9d70
Khalil Melhem <me@khalil.im>
2024-06-19 19:20:00 +0300

    enhance about section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -9,7 +9,7 @@ const About = () => {
       className="-mx-5 my-10 bg-black px-5 py-10 text-white lg:mx-auto lg:my-24 lg:rounded-[64px] lg:px-14 lg:py-24 xl:px-[8%]"
     >
       <div className="flex flex-col items-center justify-between gap-y-20 xl:flex-row-reverse">
-        <div className="flex items-center justify-center rounded-[64px] bg-white max-xl:mx-auto xl:ml-auto xl:h-[555px]">
+        <div className="flex max-w-[97.5%] items-center justify-center rounded-[64px] bg-white max-xl:mx-auto xl:ml-auto xl:h-[555px]">
           <Image
             src="/me.png"
             alt="My Memoji"
@@ -18,7 +18,7 @@ const About = () => {
             quality={100}
           />
         </div>
-        <div className="w-full space-y-10 lg:space-y-20 xl:w-[55%]">
+        <div className="w-full space-y-10 text-center xl:w-[55%] xl:text-left">
           <Text />
           <Button />
         </div>
