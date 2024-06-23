82fa1db514e07e6a421bc327a0aa5a175862dcef
Khalil Melhem <me@khalil.im>
2024-06-23 22:56:14 +0300

    enhance theme toggle animations

--- src/components/global/theme/ThemeToggle.tsx
+++ src/components/global/theme/ThemeToggle.tsx
@@ -1,14 +1,24 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
 
-import Moon from "~icons/line-md/moon-alt-to-sunny-outline-loop-transition";
-import Sun from "~icons/line-md/sunny-outline-to-moon-alt-loop-transition";
+import { useTheme } from "next-themes";
+
+import InitialMoonIcon from "~icons/line-md/moon-loop";
+import MoonIcon from "~icons/line-md/sunny-outline-to-moon-alt-loop-transition";
+
+import InitialSunIcon from "~icons/line-md/sunny-outline-loop";
+import SunIcon from "~icons/line-md/moon-alt-to-sunny-outline-loop-transition";
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
   const [key, setKey] = useState(0);
+  const [hasBeenClicked, setHasBeenClicked] = useState(false);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   useEffect(() => {
     setKey((prevKey) => prevKey + 1);
@@ -16,19 +26,32 @@ const ThemeToggle = () => {
 
   const toggleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark");
+    if (!hasBeenClicked) {
+      setHasBeenClicked(true);
+    }
   };
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <button
       type="button"
-      className="inline-flex rounded-2xl p-2 hover:bg-foreground hover:text-background"
       onClick={toggleTheme}
+      className="inline-flex rounded-2xl p-2 hover:bg-foreground hover:text-background"
     >
-      <Sun key={`sun-${key}`} className="size-6 scale-100 dark:scale-0" />
-      <Moon
-        key={`moon-${key}`}
-        className="absolute size-6 scale-0 dark:scale-100"
-      />
+      {theme === "dark" ? (
+        hasBeenClicked ? (
+          <SunIcon key={`sun-${key}`} className="size-6" />
+        ) : (
+          <InitialSunIcon key={`initial-sun-${key}`} className="size-6" />
+        )
+      ) : hasBeenClicked ? (
+        <MoonIcon key={`moon-${key}`} className="size-6" />
+      ) : (
+        <InitialMoonIcon key={`initial-moon-${key}`} className="size-6" />
+      )}
       <span className="sr-only">Toggle theme</span>
     </button>
   );
