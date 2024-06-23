19f04fd5ba1016e70ef6ad4b948669d96e133378
Khalil Melhem <me@khalil.im>
2024-06-24 01:29:01 +0300

    why was i doing that anyways?

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -15,17 +15,16 @@ const About = () => {
       className="padding-b-gone my-10 bg-foreground py-10 text-background lg:mx-auto lg:my-24 lg:rounded-8xl lg:px-14 lg:py-24 xl:px-[8%]"
     >
       <div className="flex flex-col items-center justify-between gap-y-20 xl:flex-row-reverse">
-        <div className="relative h-[350px] w-full max-w-[97.5%] overflow-hidden rounded-8xl bg-background max-xl:mx-auto sm:w-[450px] xl:h-[450px]">
-          <Image
-            src="/me.jpg"
-            alt="Image of me shot from the back at a sports competition"
-            quality={100}
-            width={1500}
-            height={1333}
-            className="h-full w-full object-cover"
-            sizes="(max-width: 640px) 97.5vw, 450px"
-          />
-        </div>
+        <Image
+          src="/me.jpg"
+          alt="Image of me shot from the back at a sports competition"
+          quality={100}
+          width={1500}
+          height={1333}
+          className="h-[350px] w-full max-w-[97.5%] rounded-8xl object-cover max-xl:mx-auto sm:w-[450px] xl:h-[450px]"
+          sizes="(max-width: 640px) 97.5vw, 450px"
+        />
+
         <div className="w-full space-y-10 text-center xl:w-[55%] xl:text-left">
           <Text />
           <Button
