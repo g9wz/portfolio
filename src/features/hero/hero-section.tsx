a794c61439e969920e54a51c7eae59ef51ac4916
Khalil Melhem <me@khalil.im>
2024-09-29 14:03:35 +0300

    style: enhance responsiveness

--- src/features/hero/hero-section.tsx
+++ src/features/hero/hero-section.tsx
@@ -1,20 +1,34 @@
+import Image from "next/image";
+
 import Links from "@/components/footer/parts/links";
 
-import WavingHand from "./assets/waving-hand";
+import WavingHand from "./assets/waving-hand.png";
+import SaudiFlag from "./assets/saudi-flag.png";
 
 const HeroSection = () => {
   return (
     <section className="mt-20 md:mt-32 lg:px-[4%] xl:px-[8%]">
-      <h1 className="text-[2rem] font-medium !leading-[4.5rem] lg:text-5xl">
+      <h1 className="text-[1.95rem] font-medium !leading-[4.5rem] lg:text-5xl">
         <div data-aos="fade-up" data-aos-delay="0">
-          <WavingHand className="-mt-3 inline-block h-[1em] w-[1em]" /> Hey, I’m
-          Khalil.
+          <Image
+            priority
+            src={WavingHand}
+            alt="Waving Hand Emoji"
+            className="-mt-3 inline-block size-[1em]"
+          />{" "}
+          Hey, I’m Khalil.
         </div>
         <div data-aos="fade-up" data-aos-delay="200">
           A Software Engineer.
         </div>
         <div data-aos="fade-up" data-aos-delay="400">
-          Currently in Riyadh, Saudi Arabia.
+          Currently in Riyadh{" "}
+          <Image
+            priority
+            src={SaudiFlag}
+            alt="Saudi Flag Emoji"
+            className="-mt-3 inline-block size-[1em]"
+          />
         </div>
       </h1>
 
